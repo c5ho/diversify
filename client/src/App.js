cc2cae21168386dc9d22660efbc7fb99ff8d6874
Calvin Ho <calvin.ch.ho@gmail.com>
2022-09-10 18:38:38 -0400

    Add list element with increasing sized boxes

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import Card from "@mui/material/Card";
+
+const Container = styled.div`
+  background: #36393e;
+
+  display: flex;
+  justify-content: center; // 1
+  flex-flow: row wrap; // 2
+  width: 100%;
+  height: 100%;
+`;
+const List = styled.div`
+  display: flex;
+  justify-content: center; // 3
+  flex-flow: column wrap; // 4
+`;
+
+// const Card = styled.div`
+//   margin: 20px;
+//   background: #fff;
+//   height: 300px;
+//   width: 400px;
+//   border-radius: 20px;
+//   box-shadow: 0 10px 20px rgba(0, 0, 0, 0.25);
+
+//   display: flex;
+//   flex-flow: column; // 5
+//   justify-content: center;
+//   align-items: center;
+// `;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      {" "}
+      <List>
+        {[0, 1, 2, 3, 5, 6, 7, 8, 9].map((item) => (
+          <Card
+            sx={{
+              margin: 2,
+              // background: #fff,
+              height: (300 * item) / 10 + 50,
+              width: 400,
+              // border-radius: 20
+              // box-shadow: 0 10px 20px rgba(0, 0, 0, 0.25);
+
+              // display: flex;
+              // flex-flow: column; // 5
+              // justify-content: center;
+              // align-items: center;
+            }}
+          >
+            {item}
+          </Card>
+        ))}
+      </List>
+    </Container>
   );
 }
 
